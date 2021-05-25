02d194020cdadc7837668ce6be827be1da000055
Tiqur <trevorbrage02@gmail.com>
2021-05-25 04:11:33 -0700

    Main title and import svg

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,20 @@
 import styles from './styles.module.css';
 import NavBar from '../../components/NavBar/NavBar.jsx';
+import Text from '../../components/Text/Text.jsx';
+import { ReactComponent as Arrow } from '../../assets/arrow.svg';
 
 const HomePage = () => {
+
   return (
     <div className={styles.mainContainer}>
       <NavBar/>
       <div className={styles.homeContainer}>
-
+        <div className={styles.heroInfo}>
+    <Text size={6} className={styles.heroTitle}>QUEEF {<Arrow fill='#51DFE0' style={{
+      height: '0.7em',
+      transform: 'rotate(90deg)'
+    }} />}</Text>
+        </div>
       </div>
     </div>
   )
