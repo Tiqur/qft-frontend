af8fd5b81eebd3c76d5fe81d576912f82df9cd9e
Tiqur <trevorbrage02@gmail.com>
2021-05-25 16:01:35 -0700

    Button spacing component

---
+++ src/components/Button.jsx
@@ -0,0 +1,5 @@
+import { styled } from '@material-ui/core/styles';
+import { spacing } from '@material-ui/system';
+import MuiButton from '@material-ui/core/Button';
+
+export default styled(MuiButton)(spacing);
