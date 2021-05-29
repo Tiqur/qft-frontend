6b6acd67ea5d01c4b70c1e38dd7ae2b1aa7c909b
Tiqur <trevorbrage02@gmail.com>
2021-05-29 16:35:24 -0700

    Update about section

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -92,11 +92,11 @@ const HomePage = () => {
       <Section class={styles.aboutContainer}>
         <Text padding='0 0 0.3em 0'size='4' id="About">About Queef Token:</Text>
         <Text color='grey' size='2'>
-          QFT is a ridiculous & friendly meme token that isn't afraid to aim high. It is being developed with multiple goals in mind. In its initial phase it will be incorporated into a direct artist-fan network and market place that will bring millions of artists and fans into the crypto world.
+          QFT is a ridiculous & friendly meme token that isn't afraid to aim high. It is being developed with multiple goals in mind. In its initial phase it will be incorporated into a direct artist-fan-vendor network and market place that will bring millions of artists and fans into the crypto world.
         </Text>
         <br/>
         <Text color='grey' size='2'>
-          In its next step it will become the driving force behind the SOST platform which is being developed to provide financial assistance to service members, veterans, EMTs, peace officers and firefighters who are struggling accessing quality healthcare.
+          In it's next step it will become the driving force behind the SOST platform, which is being developed to provide financial assistance to service members, veterans, EMTs, peace officers and firefighters who are struggling to access the care they need.
         </Text>
         <br/>
         <Text color='grey' size='2'>
@@ -104,9 +104,7 @@ const HomePage = () => {
         </Text>
         <br/>
         <Text color='grey' size='2'>
-          Everybody knows that nowadays artists get pennies on the dollar when they put their art on places like iTunes & Spotify. 
-          Every single day veterans who have sacrificed so much for us end up on the streets, not being able to access to the quality care they need.
-          QFT aims to take on such big issues and change the world in a very meaningful way.
+          Everyone knows that nowadays artists get pennies on the dollar when they put their art on places like iTunes & Spotify. Every single day veterans who have sacrificed so much for us all end up homeless on the streets, not being able to access the quality care they need. QFT aims to take on these big issues and change the world in a very meaningful way.
         </Text>
       </Section>
     
